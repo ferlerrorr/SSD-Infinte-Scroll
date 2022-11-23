ba2bcb7932aeb46ebfec8f8f11a1c95201cba83b
Ferl Jhon D. Javier <80177329+ferlerrorr@users.noreply.github.com>
2022-11-23 10:31:03 +0800

    Page Optimizations
    lighthouse test

---
+++ script.js
@@ -0,0 +1,48 @@
+const gridContainer = document.querySelector(".grid-container");
+const watcher = document.querySelector(".intersection-watcher");
+let url = "http://127.0.0.1:8000/api/devprod/";
+let page = 1;
+
+const fetchData = async () => {
+
+  // const result = await fetch(url + "?page="+ page++);
+  const result = await fetch(url);
+  const data = await result.json();
+
+  // return (data.data);  
+  return (data);  
+
+};
+
+const addContent = async () => {
+  const data = await fetchData()
+  data.forEach(post => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `<h2>${post.title}</h2><img style="width:100px;" src="${post.image.src}" alt=""> <button class="ato-cart" style="padding: 6px 6px 6px 6px; font-size:.6rem;"> Add to Cart <span class="material-symbols-outlined btn-icon" style="font-size: 15px; span:0; margin:0" >
+    add_circle
+    </span> </button> `;
+    gridContainer.appendChild(card); 
+  });
+};
+
+const handleIntersect = entries => {
+  // console.log(entries);
+  if (entries[0].isIntersecting) {
+    addContent();
+  }
+};
+
+const options = {
+  // root: null
+  // threshold: 1
+  // rootMargin: "-20% 0px"
+}
+new IntersectionObserver(handleIntersect, options).observe(watcher);
+
+
+// Ancienne façon de faire peu performante
+// window.addEventListener("scroll", () => {
+  
+//   console.log(document.body.getBoundingClientRect());
+// })
