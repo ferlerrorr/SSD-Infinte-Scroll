f75630416883dbdf531f8516e143d82848e2c3d4
Ferl Jhon D. Javier <80177329+ferlerrorr@users.noreply.github.com>
2022-11-24 18:36:51 +0800

    Url Change
    route url change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const gridContainer = document.querySelector(".grid-container");
 const watcher = document.querySelector(".intersection-watcher");
-let url = "http://127.0.0.1:8000/api/devprod/";
+let url = "http://127.0.0.1:8000/api/shopify/all-products/";
 let page = 1;
 
 const fetchData = async () => {
@@ -20,9 +20,9 @@ const addContent = async () => {
     <div class="card-container">
     <h2>${post.title}</h2>
     <img style="width:100px;" src="${post.image.src}" alt="">
-    <p>${post.variants[0].price}</p> 
-    <button class="ato-cart"> Add to Cart 
-    <span class="material-symbols-outlined btn-icon">
+    <p>₱${post.variants[0].price}</p> 
+    <button class="ato-cart" value="${post.variants[0].id}" > Add to Cart 
+    <span class="material-symbols-outlined btn-icon" >
     add_circle
     </span>
     </div>
